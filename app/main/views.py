75797dd004997ea09d15ec94b8565e4f907d2fae
jerushaotieno <jerushaotienocoding@gmail.com>
2022-03-05 14:20:07 +0300

    introduce routing for pitches and categories

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,11 @@ from flask import render_template
 
 def landing_page():
     return render_template('index.html') 
+
+@app.route('/pitches')
+def home():
+    return render_template('pitches.html')
+
+@app.route('/pitches/<category>')
+def categories(category):
+    return render_template('categories.html') 
