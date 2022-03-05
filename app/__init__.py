38ebfc0cfcd67575d11ccd52b7af4a0ff48dc647
jerushaotieno <jerushaotienocoding@gmail.com>
2022-03-05 15:54:09 +0300

    create a pitch class in the models.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from config import DevConfig
 
+#App initialization
 app = Flask(__name__)
+
+# Configuration setup
+app.config.from_object(DevConfig)
 
 from app import views 
---
+++ app/models.py
@@ -0,0 +1,9 @@
+class Pitch:
+    '''
+    '''
+    def __init__(self, pitch_text, pitch_category, pitch_author):
+        '''
+        '''
+        self.pitch_text=pitch_text
+        self.pitch_category=pitch_category
+        self.pitch_author=pitch_author
