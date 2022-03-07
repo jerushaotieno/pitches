d95914a6aa5cfb089d77d53d5772894264516ec6
jerushaotieno <jerushaotienocoding@gmail.com>
2022-03-07 05:09:57 +0300

    initialize flask-login to manage the user authentication system

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,11 @@ from config import DevConfig
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 # #App initialization
 # app = Flask(__name__)
@@ -27,6 +32,11 @@ db=SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
+
+    #Initializing Flask Extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
 
     #set up
     app.config.from_object(config_options[config_name])
