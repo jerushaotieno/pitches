cc9f59cf962952d41d98da200c26bcd6c2e09eac
jerushaotieno <jerushaotienocoding@gmail.com>
2022-03-07 08:41:56 +0300

    create registration/login route and render the registration form with wtf.quick_form() function

--- app/main/views.py
+++ app/main/views.py
@@ -14,9 +14,14 @@
 # def categories(category):
 #     return render_template('categories.html') 
 
+from app import auth
 from . import main
 from flask import render_template
 from flask_login import login_required
+from flask import render_template,redirect,url_for
+from ..models import User
+from .forms import RegistrationForm
+from .. import db
 
 @main.route('/')
 def landing_page():
@@ -34,4 +39,15 @@ def categories():
 @login_required
 def pitch_comments(pitches_id):
     comments = pitch_comments.get_comments(pitches_id)
-    return render_template('pitches.html')
+    return render_template('pitches.html', comments=comments)
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
