901abf210442436f80201582099a6de3868fc9a5
jerushaotieno <jerushaotienocoding@gmail.com>
2022-03-07 10:53:51 +0300

    create login/logout to give access app features

--- app/main/views.py
+++ app/main/views.py
@@ -41,13 +41,3 @@ def pitch_comments(pitches_id):
     comments = pitch_comments.get_comments(pitches_id)
     return render_template('pitches.html', comments=comments)
 
-@auth.route('/register',methods = ["GET","POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
