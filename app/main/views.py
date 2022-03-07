496e9ffc17a37afbdd18ec593505b065627410eb
jerushaotieno <jerushaotienocoding@gmail.com>
2022-03-07 12:41:06 +0300

    create code to edit profile

--- app/main/views.py
+++ app/main/views.py
@@ -20,7 +20,7 @@ from flask import render_template
 from flask_login import login_required
 from flask import render_template,request,redirect,url_for,abort
 from ..models import Reviews, User
-from .forms import RegistrationForm
+from .forms import RegistrationForm, ReviewForm, UpdateProfile
 from .. import db
 
 @main.route('/')
@@ -41,3 +41,21 @@ def pitch_comments(pitches_id):
     comments = pitch_comments.get_comments(pitches_id)
     return render_template('pitches.html', comments=comments)
 
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
