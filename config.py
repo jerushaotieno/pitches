ed9fde3a1ceb2338978d4a29eacee0d58b2cb4a1
jerushaotieno <jerushaotienocoding@gmail.com>
2022-03-07 03:30:34 +0300

    create database migrations

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     
     SECRET_KEY=os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:0@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:Access@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
