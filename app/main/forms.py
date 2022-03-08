fde22ff91c2e5204a75217f6c06cbd9816a82f51
jerushaotieno <jerushaotienocoding@gmail.com>
2022-03-08 12:52:35 +0300

    create a profile page and add code to edit profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,8 @@
-# class UpdateProfile(FlaskForm):
-#     bio = TextAreaField('Tell us about you.',validators = [Required()])
-#     submit = SubmitField('Submit')
+from wtforms import SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [DataRequired()])
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,26 +1,10 @@
-# from app import app
-# from flask import render_template
-
-# @app.route('/')
-
-# def landing_page():
-#     return render_template('index.html') 
-
-# @app.route('/pitches')
-# def home():
-#     return render_template('pitches.html')
-
-# @app.route('/pitches/<category>')
-# def categories(category):
-#     return render_template('categories.html') 
-
 from app import auth
 from . import main
 from flask import render_template
 from flask_login import login_required
 from flask import render_template,request,redirect,url_for,abort
 from ..models import User
-# from .forms import RegistrationForm, LoginForm
+from .forms import UpdateProfile
 from .. import db
 
 @main.route('/')
@@ -31,42 +15,30 @@ def index():
 def home():
     return render_template('home.html')
 
-# @main.route('/pitches/<category>')
-# def categories():
-#     return render_template('categories.html') 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
 
-# @main.route('/comments/<int:pitches_id>', methods=['GET','POST'])
-# @login_required
-# def pitch_comments(pitches_id):
-#     comments = pitch_comments.get_comments(pitches_id)
-#     return render_template('pitches.html', comments=comments)
+    if user is None:
+        abort(404)
 
-# @main.route('/user/<uname>/update',methods = ['GET','POST'])
-# @login_required
-# def update_profile(uname):
-#     user = User.query.filter_by(username = uname).first()
-#     if user is None:
-#         abort(404)
+    return render_template("profile/profile.html", user = user)
 
-    # form = UpdateProfile()
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
 
-    # if form.validate_on_submit():
-    #     user.bio = form.bio.data
+    form = UpdateProfile()
 
-    #     db.session.add(user)
-    #     db.session.commit()
+    if form.validate_on_submit():
+        user.bio = form.bio.data
 
-    #     return redirect(url_for('.profile',uname=user.username))
+        db.session.add(user)
+        db.session.commit()
 
-    # return render_template('profile/update.html',form =form)
+        return redirect(url_for('.profile',uname=user.username))
 
-# @main.route('/user/<uname>/update/pic',methods= ['POST'])
-# @login_required
-# def update_pic(uname):
-#     user = User.query.filter_by(username = uname).first()
-#     if 'photo' in request.files:
-#         filename = photos.save(request.files['photo'])
-#         path = f'photos/{filename}'
-#         user.profile_pic_path = path
-#         db.session.commit()
-#     return redirect(url_for('main.profile',uname=uname))
+    return render_template('profile/update.html',form =form)
